9537604fffa5c6d19776620c555b72e36bcc5414
slzander <staceyzander@gmail.com>
2020-02-28 08:41:00 -0700

    finishes routing for app

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,23 @@
 <template>
     <div class="ui secondary pointing menu">
-        <a href="/" class="active item">
+        <router-link to="/" class="active item">
             Image Storage
-        </a>
+        </router-link>
 
         <div class="right menu">
-            <a href="#" class="ui item" @click="login">
+            <div v-if="isLoggedIn" class="horizontal">
+                <router-link to="/" class="item" >
+                    Galleries
+                </router-link>
+                <router-link to="/upload" class="item" >
+                    Upload
+                </router-link>
+                <a href="#" class="item" @click="logout">
+                    Logout
+                </a>
+            </div>
+
+            <a v-else href="#" class="ui item" @click="login">
                 Login
             </a>
         </div>
@@ -13,10 +25,18 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 export default {
     name: 'AppHeader',
-    methods: mapActions(['login'])
+    computed: mapGetters(['isLoggedIn']),
+    methods: mapActions(['login', 'logout'])
 }
 </script>
+
+<style scoped>
+.horizontal {
+    display: flex;
+    flex-direction: row;
+}
+</style>
